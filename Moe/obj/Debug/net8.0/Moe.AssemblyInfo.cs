d7e9c337e52b4e8ffb8fb3ec4511873a26e3e3aa
Alex <Alex@alex>
2024-09-26 13:18:22 +0500

    Finish Him

--- Moe/obj/Debug/net8.0/Moe.AssemblyInfo.cs
+++ Moe/obj/Debug/net8.0/Moe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df55468ece561c911629f96baceced0b2ff0664")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
